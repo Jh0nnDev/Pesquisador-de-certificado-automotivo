93899f851de64d1160932f95ab0af89fd6dbdad6
Jh0nnDev <joaosouzzaoliveira@gmail.com>
2025-09-18 16:11:56 -0300

    Teste exponsive pt1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bnt= document.querySelector("#Button");
+﻿const bnt= document.querySelector("#Button");
 
 bnt.addEventListener("click", function (e)
 //função para ser rodada após o click do botão
@@ -9,20 +9,18 @@ bnt.addEventListener("click", function (e)
     const Numero = document.querySelector("#NumbOrd");
     const Carro = Numero.value;
     //armazena o valor do numero de ordem na variavel "carro" 
-    console.log(Carro)
 
     let opc1 = document.getElementById("CRLV")
     const CRLV = opc1.value;
     let opc2 = document.getElementById("TCO")
     const TCO = opc2.value;
     if(opc1.checked) 
-        window.open("https://www.google.com/" + Carro + "/" + CRLV , '_blank');
+        window.open("https://expvitoria.com/certificados/" + CRLV + "/" + Carro + ".pdf" , '_blank');
         else if(opc2.checked) 
-            window.open("https://www.google.com/" + Carro + "/" + TCO , '_blank');
+            window.open("https://expvitoria.com/certificados/" + TCO + "/" + Carro + ".pdf"  , '_blank');
             else alert("Marca alguma ai corno")
 
     
 })
-
 
 
